b3c81fdff634929eca1854ca5137f64a5842fb7f
zawyehtut440 <leoho202@gmail.com>
2025-01-24 13:40:09 +0800

    added flag to prompt user input the domain website.

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"sitemap/sitemap_builder"
 )
 
 var _ = fmt.Println
 
 func main() {
-	fmt.Println(sitemap_builder.CreateSitemap("https://www.calhoun.io"))
+	var websiteDomain string
+	flag.StringVar(&websiteDomain, "domain", "", "the website domain, have to provide full url, like https://www.google.com.")
+	flag.Parse()
+	if websiteDomain == "" {
+		fmt.Println("you didn't provide website domain.")
+		os.Exit(65)
+	}
+
+	fmt.Println(sitemap_builder.CreateSitemap(websiteDomain))
+
+	// fmt.Println(sitemap_builder.CreateSitemap("https://www.calhoun.io"))
 	// fmt.Println(sitemap_builder.CreateSitemap("https://www.larstornoe.com"))
 	// fmt.Println(sitemap_builder.CreateSitemap("https://gobyexample.com/"))
 }
